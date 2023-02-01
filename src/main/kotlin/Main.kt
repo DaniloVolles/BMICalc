be5b87c9cd53e6b5cbefa1062574e00fff413230
Danilo Volles <danilo.volles@outlook.com>
2023-02-01 19:46:18 -0300

    Program first version.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,25 +1,39 @@
 import java.util.Scanner
 
 // Variable initialization
-    private val input = Scanner(System.`in`)
-    var height: Int = 0
-    var weight: Int = 0
 
-fun main() {
+fun main(args: Array<String>) {
+
+    val input = Scanner(System.`in`)
 
     // Presenting the program
     println("Be very welcome to the BMI calculator!\n" +
-            "Please, insert the data when asked:\n")
+            "Please, insert the data when asked.")
 
     // Input data
-    println("Insert your height in centimeters:")
-    var height = input.nextInt()
-
+    println("Insert your height in meters:")
+    var height = input.nextDouble()
     println("Insert your weight in kilograms:")
-    var weight = input.nextInt()
+    var weight = input.nextDouble()
 
     // Output data
     println("Your height is $height cm and your weight is $weight kg.")
 
+    // BMI calculation
+    var bmi = weight/(height*height)
 
+    val initialText = "Your BMI index is $bmi and you are"
+
+    // Flow - Here I will say what stage they are in the BMI scale
+    if (bmi < 18.5) {
+        println("$initialText underweight")
+    } else if (bmi < 25) {
+        println("$initialText normal weight")
+    } else if (bmi < 30) {
+        println("$initialText overweight")
+    } else if (bmi < 35) {
+        println("$initialText obese")
+    } else {
+        println("$initialText extremely obese")
+    }
 }
