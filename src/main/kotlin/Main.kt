3abe24f38fc3bfb7fb911dd1f47a4a274af7a7ad
Danilo Volles <danilo.volles@outlook.com>
2023-02-02 19:04:57 -0300

    Code formatting. Var -> val. removing spaces.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,23 +4,21 @@ import java.util.Scanner
 
 fun main() {
 
-
     // Presenting the program
     println("Be very welcome to the BMI calculator!\n" +
             "Please, insert the data when asked.")
 
     // Input data
     println("Insert your height in meters:")
-    var height = input.nextDouble()
+    val height = input.nextDouble()
     println("Insert your weight in kilograms:")
-    var weight = input.nextDouble()
+    val weight = input.nextDouble()
 
     // Output data
     println("Your height is $height meters and your weight is $weight kilograms.")
 
-    // BMI calculation
-    var bmi = bmiCalculation(height, weight)
-    var result = bmiResult(bmi)
+    val bmi = bmiCalculation(height, weight)
+    val result = bmiResult(bmi)
 
     println(result)
 }
