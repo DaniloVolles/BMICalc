3e79d60fd050ab8eb391ffd69123429ee10c0f18
Danilo Volles <danilo.volles@outlook.com>
2023-02-06 17:54:46 -0300

    Implementation of menu command!

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,27 @@
 fun main() {
 
+    println("Be very welcome to the BMI calculator!")
+
     // Presenting the program
-    println("Be very welcome to the BMI calculator!\n" +
-            "Please, insert the data when asked.")
+    val menuText = "What do you want to do?\n" +
+            "- 1 - Calculate BMI\n" +
+            "- 0 - Terminate program"
+
+    println(menuText)
+
+    var menuOption = readln().toInt()
+    while (menuOption != 0) {
+
+        when (menuOption) {
+            1 -> calculateBMI()
+        }
+        println(menuText)
+        menuOption = readln().toInt()
+    }
+
+}
+
+fun calculateBMI() {
 
     // Input data
     println("Insert your height in meters:")
@@ -17,6 +36,7 @@ fun main() {
     val result = bmiResult(bmi)
 
     println(result)
+
 }
 
 // Implementar menu
